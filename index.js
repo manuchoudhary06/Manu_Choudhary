9e48e0635105b48cf6d7112b6ffcc61fee27d33a
manuchoudhary06 <manuchoudhary0204@gmail.com>
2024-03-20 22:51:21 +0530

    about section

--- index.js
+++ index.js
@@ -14,7 +14,53 @@ const interval = setInterval(() => {
   const days = Math.floor(
     (elapsedTime % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24)
   );
-
-  // Display the countup
   countup.innerHTML = `<div class="exp">Crafting Frontend Experiences for <em>${years}</em> years <em>${months}</em> months and <em>${days}</em> days</div>`;
 }, 1000);
+
+// grid colouring on scroll
+const gridItems = document.querySelectorAll(".grid-item");
+
+window.addEventListener("scroll", function () {
+  const scrollPosition = window.scrollY;
+
+  gridItems.forEach(function (item, index) {
+    const offsetTop = item.offsetTop;
+    const offsetBottom = offsetTop + item.offsetHeight;
+    const text = item.querySelector("p");
+    const quote = item.querySelector(".quote");
+
+    if (scrollPosition >= offsetTop && scrollPosition < offsetBottom) {
+      item.classList.add("active");
+      if (text) {
+        const percent = Math.min(
+          1,
+          Math.max(0, (scrollPosition - offsetTop) / (offsetBottom - offsetTop))
+        );
+        text.style.color = interpolateColor(percent);
+        if (quote) {
+          quote.style.opacity = 1;
+        }
+      }
+    } else {
+      item.classList.remove("active");
+      if (text) {
+        text.style.color = ""; // Reset color
+        if (quote) {
+          quote.style.opacity = 0;
+        }
+      }
+    }
+  });
+});
+
+function interpolateColor(percent) {
+  const startColor = [255, 255, 255]; // Start color (white)
+  const endColor = [37, 37, 37, 0.7]; // End color (orange)
+
+  const interpolatedColor = startColor.map((channel, index) => {
+    const range = endColor[index] - channel;
+    return Math.round(channel + range * percent);
+  });
+
+  return `rgb(${interpolatedColor.join(",")})`;
+}
