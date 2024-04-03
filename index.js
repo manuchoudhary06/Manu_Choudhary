fa82de7904fa98cd0ee35a16b1f71a31027ee453
manuchoudhary06 <manuchoudhary0204@gmail.com>
2024-04-03 22:50:20 +0530

    mobile devices

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ const countup = document.querySelector(".experience-time");
 // Set the start date (January 1, 2020)
 const startDate = new Date("2020-01-01T00:00:00").getTime();
 
-const interval = setInterval(() => {
-  const now = new Date().getTime();
-  const elapsedTime = now - startDate;
+const now = new Date().getTime();
+const elapsedTime = now - startDate;
 
-  const years = Math.floor(elapsedTime / (1000 * 60 * 60 * 24 * 365));
-  const months = Math.floor(
-    (elapsedTime % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30.44)
-  );
-  const days = Math.floor(
-    (elapsedTime % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24)
-  );
-  countup.innerHTML = `<div class="exp">Crafting Frontend Experiences for <em>${years}</em> years <em>${months}</em> months and <em>${days}</em> days</div>`;
-}, 1000);
+const years = Math.floor(elapsedTime / (1000 * 60 * 60 * 24 * 365));
+const months = Math.floor(
+  (elapsedTime % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30.44)
+);
+const days = Math.floor(
+  (elapsedTime % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24)
+);
+countup.innerHTML = `<div class="exp">Crafting Frontend Experiences for <em>${years}</em> years <em>${months}</em> months and <em>${days}</em> days</div>`;
 
 // grid colouring on scroll
 const gridItems = document.querySelectorAll(".grid-item");
